eb3d87cd125fe58280613a9ef9b4bd9c8bfe814e
MartinNeifert <34620576+MartinNeifert@users.noreply.github.com>
2019-02-05 18:47:43 -0800

    Test submod add

--- TriangleTexture/TriangleTexture.js
+++ TriangleTexture/TriangleTexture.js
@@ -2,11 +2,12 @@ var rows = 5;
 var cols = 5;
 var fields = new Array(rows * cols)
 var fieldSize = 70
-var xOff = 20
-var yOff = 20
+var xOff = 50
+var yOff = 50
 
 function setup(){
-  createCanvas(1000, 1000);
+  var canvas = createCanvas(500, 500);
+  canvas.parent('TriangleTexture');
   var n = 0;
   
   for(var i = 0; i < rows; i++){
@@ -19,7 +20,6 @@ function setup(){
 
 function draw(){
   translate(mouseX/15 + xOff, mouseY/15 + yOff);
-  applyMatrix(1, 0, 0, 1, mouseX/2000, 0);
   clear();
   for(var i = 0; i < fields.length; i++){
     fields[i].step();
